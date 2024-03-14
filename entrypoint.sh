7f350b68eb77ee365c4a307829fe65bb5f93c6dc
Wai LyanPyae <110360470+AlexLyanPyae@users.noreply.github.com>
2024-03-14 01:09:13 +0000

    entrypoint sh typo fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,20 @@
 
 echo "========================"
 
+# Setting Git user name
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email"${INPUT_EMAIL}"
-git config --global --add sfafe.directory /github/workspace
 
+# Correcting the setting of Git user email with added space
+git config --global user.email "${INPUT_EMAIL}"
+
+# Marking the workspace as a safe directory for Git operations
+git config --global --add safe.directory /github/workspace
+
+# Running your Python script. Make sure the path and script name are correct.
 python3 /usr/bin/feed.py
 
+# Adding all changes to Git staging area, committing, and pushing to the repository
 git add -A && git commit -m "Update Feed"
-
 git push --set-upstream origin main
 
 echo "========================"
